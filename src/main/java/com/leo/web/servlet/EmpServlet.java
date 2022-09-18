e040bd69ed8d51a359c6b7c88bd8eae16be4806f
leo <3181538941@qq.com>
2022-09-18 20:43:56 +0800

    修复非单行json读取问题, 员工更改个人信息接口编码为utf-8

--- src/main/java/com/leo/web/servlet/EmpServlet.java
+++ src/main/java/com/leo/web/servlet/EmpServlet.java
@@ -24,18 +24,22 @@ public class EmpServlet extends BaseServlet {
                       HttpServletResponse res) throws ServletException, IOException {
         //1. 接收数据
         BufferedReader br = req.getReader();
-        String params = br.readLine();//json字符串
+        StringBuilder params = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            params.append(line);
+        }
         logger.info("req read: {}", params);
         //转为Employee对象
-        Employee employee = JSON.parseObject(params, Employee.class);
-        if (employee == null) {
-            logger.error("json解析错误: {}", br.read());
+        Employee emp = JSON.parseObject(String.valueOf(params), Employee.class);
+        if (emp == null) {
+            logger.error("json解析错误: {}", params);
             res.getWriter().write("err");
             return;
         }
-        logger.info("parsed employee: emp.id: {}, emp.passwd: {}", employee.getId(), employee.getPasswd());
+        logger.info("parsed employee: emp.id: {}, emp.passwd: {}", emp.getId(), emp.getPasswd());
         // 查询
-        Employee loginEmp = employeeService.login(employee.getId(), employee.getPasswd());
+        Employee loginEmp = employeeService.login(emp.getId(), emp.getPasswd());
         if (loginEmp != null) {
             logger.info("登录成功");
             logger.info("selected employee: {}", loginEmp);
@@ -63,14 +67,30 @@ public class EmpServlet extends BaseServlet {
     
     public void updateSelfInfo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //1. 接收数据
+        request.setCharacterEncoding("UTF-8");
         BufferedReader br = request.getReader();
-        String params = br.readLine();
+        StringBuilder params = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            params.append(line);
+        }
         logger.info("json: {}", params);
         //转为Employee对象
-        Employee employee = JSON.parseObject(params, Employee.class);
-        System.out.println(employee);
+        Employee emp = JSON.parseObject(params.toString(), Employee.class);
+        if (emp == null) {
+            logger.error("json解析错误: {}", params);
+            response.getWriter().write("err");
+            return;
+        }
+        // 通过编号和密码查询员工信息, 判断是否合法
+        Employee selectEmp = employeeService.login(emp.getId(), emp.getPasswd());
+        if (selectEmp == null) {
+            logger.info("密码错误: {}", emp);
+            response.getWriter().write("false");
+            return;
+        }
         //2. 调用service更新
-        employeeService.updateEmp(employee);
+        employeeService.updateEmp(emp);
         //3. 返回结果
         response.setContentType("text/json;charset=utf-8");
         response.getWriter().write("{\"msg\":\"success\"}");
